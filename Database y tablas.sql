0092a2ab347b1375ad6a0ac974bf5e1366883006
camilo09936 <camilo09936@gmail.com>
2026-05-28 21:18:17 -0500

    Add files via upload
    Actualización Creacion BD y Tablas

--- Database y tablas.sql
+++ Database y tablas.sql
@@ -57,12 +57,6 @@ CREATE TABLE SEDE (
 	direccion VARCHAR(200) NOT NULL,
 	telefono VARCHAR(100) NOT NULL,
 	);
-INSERT INTO SEDE(nombre, direccion, telefono) VALUES
-('Sede Norte', 'Calle 10 #20-30', '6041111111'),
-('Sede Sur', 'Carrera 15 #40-20', '6042222222'),
-('Sede Centro', 'Avenida 30 #12-10', '6043333333'),
-('Sede Bello', 'Calle 45 #22-18', '6044444444'),
-('Sede Itagui', 'Carrera 50 #33-12', '6045555555');
 
 CREATE TABLE DISENADOR (
 	id_disenador INT PRIMARY KEY IDENTITY(1,1),
@@ -72,14 +66,6 @@ CREATE TABLE DISENADOR (
 	id_sede INT NOT NULL,
 	CONSTRAINT FK_disenador_sede FOREIGN KEY (id_sede) REFERENCES SEDE(id_sede),
 );
-INSERT INTO DISENADOR(nombre, apellido, correo, id_sede) VALUES
-('Daniel', 'Ruiz', 'daniel@gmail.com', 1),
-('David', 'Gómez', 'David@gmail.com', 1),
-('Laura', 'Mora', 'laura@gmail.com', 2),
-('Dayana', 'Correa', 'Dayana@gmail.com', 2),
-('Felipe', 'Castro', 'felipe@gmail.com', 3),
-('Valentina', 'Gil', 'vale@gmail.com', 4),
-('Sebastian', 'Rios', 'sebastian@gmail.com', 5);
 
 CREATE TABLE BLOQUE_HORARIO(
     id_bloque INT PRIMARY KEY IDENTITY(1,1),
@@ -102,49 +88,6 @@ CREATE TABLE AGENDA_DISENADOR(
     CONSTRAINT FK_agenda_disenador FOREIGN KEY(id_disenador) REFERENCES DISENADOR(id_disenador),
     CONSTRAINT FK_agenda_bloque FOREIGN KEY(id_bloque) REFERENCES BLOQUE_HORARIO(id_bloque)
 );
-INSERT INTO AGENDA_DISENADOR(id_disenador, dia_semana, id_bloque) VALUES
-(1, 'Monday', 1), (1, 'Monday', 2), (1, 'Monday', 3), (1, 'Monday', 4), (1, 'Monday', 5), 
-(1, 'Tuesday', 1),(1, 'Tuesday', 2),(1, 'Tuesday', 3),(1, 'Tuesday', 4),(1, 'Tuesday', 5), 
-(1, 'Wednesday', 1),(1, 'Wednesday', 2),(1, 'Wednesday', 3),(1, 'Wednesday', 4),(1, 'Wednesday', 5),
-(1, 'Thursday', 1),(1, 'Thursday', 2),(1, 'Thursday', 3),(1, 'Thursday', 4),(1, 'Thursday', 5),
-(1, 'Friday', 1),(1, 'Friday', 2),(1, 'Friday', 3),(1, 'Friday', 4),(1, 'Friday', 5),
-(1, 'Saturday', 1),(1, 'Saturday', 2),(1, 'Saturday', 3),
-(2, 'Monday', 2),(2, 'Monday', 3),(2, 'Monday', 4),(2, 'Monday', 5),(2, 'Monday', 6),
-(2, 'Tuesday', 2),(2, 'Tuesday', 3),(2, 'Tuesday', 4),(2, 'Tuesday', 5),(2, 'Tuesday', 6),
-(2, 'Wednesday', 2),(2, 'Wednesday', 3),(2, 'Wednesday', 4),(2, 'Wednesday', 5),(2, 'Wednesday', 6),
-(2, 'Thursday', 2),(2, 'Thursday', 3),(2, 'Thursday', 4),(2, 'Thursday', 5),(2, 'Thursday', 6),
-(2, 'Friday', 2),(2, 'Friday', 3),(2, 'Friday', 4),(2, 'Friday', 5),(2, 'Friday', 6),
-(2, 'Saturday', 2),(2, 'Saturday', 3),
-(3, 'Monday', 2),(3, 'Monday', 3),(3, 'Monday', 4),(3, 'Monday', 5),(3, 'Monday', 6),
-(3, 'Tuesday', 2),(3, 'Tuesday', 3),(3, 'Tuesday', 4),(3, 'Tuesday', 5),(3, 'Tuesday', 6),
-(3, 'Wednesday', 2),(3, 'Wednesday', 3),(3, 'Wednesday', 4),(3, 'Wednesday', 5),(3, 'Wednesday', 6),
-(3, 'Thursday', 2),(3, 'Thursday', 3),(3, 'Thursday', 4),(3, 'Thursday', 5),(3, 'Thursday', 6),
-(3, 'Friday', 2),(3, 'Friday', 3),(3, 'Friday', 4),(3, 'Friday', 5),(3, 'Friday', 6),
-(3, 'Saturday', 2),(3, 'Saturday', 3),
-(4, 'Monday', 3),(4, 'Monday', 4),(4, 'Monday', 5),(4, 'Monday', 6),
-(4, 'Tuesday', 3),(4, 'Tuesday', 4),(4, 'Tuesday', 5),(4, 'Tuesday', 6),
-(4, 'Wednesday', 3),(4, 'Wednesday', 4),(4, 'Wednesday', 5),(4, 'Wednesday', 6),
-(4, 'Thursday', 3),(4, 'Thursday', 4),(4, 'Thursday', 5),(4, 'Thursday', 6),
-(4, 'Friday', 3),(4, 'Friday', 4),(4, 'Friday', 5),(4, 'Friday', 6),
-(4, 'Saturday', 2),(4, 'Saturday', 3),
-(5, 'Monday', 1),(5, 'Monday', 2),(5, 'Monday', 3),(5, 'Monday', 4),(5, 'Monday', 5),(5, 'Monday', 6),
-(5, 'Tuesday', 1),(5, 'Tuesday', 2),(5, 'Tuesday', 3),(5, 'Tuesday', 4),(5, 'Tuesday', 5),(5, 'Tuesday', 6),
-(5, 'Wednesday', 1),(5, 'Wednesday', 2),(5, 'Wednesday', 3),(5, 'Wednesday', 4),(5, 'Wednesday', 5),(5, 'Wednesday', 6),
-(5, 'Thursday', 1),(5, 'Thursday', 2),(5, 'Thursday', 3),(5, 'Thursday', 4),(5, 'Thursday', 5),(5, 'Thursday', 6),
-(5, 'Friday', 1),(5, 'Friday', 2),(5, 'Friday', 3),(5, 'Friday', 4),(5, 'Friday', 5),
-(5, 'Saturday', 1),(5, 'Saturday', 2),(5, 'Saturday', 3),(5, 'Saturday', 4),
-(6, 'Monday', 1),(6, 'Monday', 2),(6, 'Monday', 3),
-(6, 'Tuesday', 1),(6, 'Tuesday', 2),(6, 'Tuesday', 3),
-(6, 'Wednesday', 1),(6, 'Wednesday', 2),(6, 'Wednesday', 3),
-(6, 'Thursday', 1),(6, 'Thursday', 2),(6, 'Thursday', 3),
-(6, 'Friday', 1),(6, 'Friday', 2),(6, 'Friday', 3),
-(6, 'Saturday', 1),(6, 'Saturday', 2),
-(7, 'Monday', 4),(7, 'Monday', 5),(7, 'Monday', 6),
-(7, 'Tuesday', 4),(7, 'Tuesday', 5),(7, 'Tuesday', 6),
-(7, 'Wednesday', 4),(7, 'Wednesday', 5),(7, 'Wednesday', 6),
-(7, 'Thursday', 4),(7, 'Thursday', 5),(7, 'Thursday', 6),
-(7, 'Friday', 4),(7, 'Friday', 5),(7, 'Friday', 6),
-(7, 'Saturday', 4),(7, 'Saturday', 5);
 
 CREATE TABLE CITA (
     id_cita INT IDENTITY(1,1) PRIMARY KEY,
@@ -193,67 +136,3 @@ CREATE TABLE PQRS (
 	CONSTRAINT FK_pqrs_estado FOREIGN KEY (id_estado_pqrs) REFERENCES ESTADO_PQRS(id_estado_pqrs),
 	CONSTRAINT FK_pqrs_usuario FOREIGN KEY (documento) REFERENCES USUARIO(documento)
 );
-
-CREATE PROCEDURE sp_AgendarCita
-    @fecha DATE,
-    @id_bloque INT,
-    @id_estado_cita INT,
-    @documento VARCHAR(20),
-    @id_sede INT,
-    @id_disenador INT
-AS
-BEGIN
-    SET NOCOUNT ON;
-
-    -- 1. VALIDACIÓN: El cliente ya tiene una cita en ese mismo bloque y fecha?
-    IF EXISTS (
-        SELECT 1 FROM CITA 
-        WHERE fecha = @fecha 
-          AND id_bloque = @id_bloque 
-          AND documento = @documento
-    )
-    BEGIN
-        RAISERROR ('Error: Ya tienes otra cita agendada en este mismo día y bloque horario.', 16, 1);
-        RETURN;
-    END
-
-    -- 2. VALIDACIÓN CRÍTICA: El diseñador ya está ocupado en ese bloque y fecha?
-    IF EXISTS (
-        SELECT 1 FROM CITA 
-        WHERE fecha = @fecha 
-          AND id_bloque = @id_bloque 
-          AND id_disenador = @id_disenador
-    )
-    BEGIN
-        -- El estado de severidad 16 fuerza a Node.js / Sequelize a capturarlo en el catch
-        RAISERROR ('Error: El diseñador seleccionado ya se encuentra asignado a otra cita en este bloque horario.', 16, 1);
-        RETURN;
-    END
-
-    -- 3. INSERCIÓN: Si pasa las validaciones anteriores, se registra la cita con éxito
-    BEGIN TRY
-        INSERT INTO CITA (fecha, id_bloque, id_estado_cita, documento, id_sede, id_disenador)
-        VALUES (@fecha, @id_bloque, @id_estado_cita, @documento, @id_sede, @id_disenador);
-        
-        PRINT 'Cita agendada exitosamente en el sistema.';
-    END TRY
-    BEGIN CATCH
-        DECLARE @ErrorMessage NVARCHAR(4000) = ERROR_MESSAGE();
-        DECLARE @ErrorSeverity INT = ERROR_SEVERITY();
-        DECLARE @ErrorState INT = ERROR_STATE();
-
-        RAISERROR (@ErrorMessage, @ErrorSeverity, @ErrorState);
-    END CATCH
-END;
-GO
-
- --PRUEBAS
-EXEC sp_agendar_cita
-    @fecha = '2026-05-18',
-    @id_bloque = 1,
-    @documento = '1001',
-    @id_sede = 1,
-    @id_disenador = 1;
-
-SELECT * FROM CITA; 
-SELECT * FROM USUARIO;
